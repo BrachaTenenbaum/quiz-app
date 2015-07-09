b319e379c3b2c27a87f973149c0f56d43363f698
bracharoths <bracha.b.rothschild@gmail.com>
2015-07-08 20:22:39 -0600

    first commit

---
+++ quiz-app/app.js
@@ -0,0 +1,16 @@
+// one question is displayed at a time
+
+//if wrong answer, then message pops up with link to chapter to learn again
+
+//if correct, music plays and old quesion fades out, new quesion fades in
+
+//when complete game- pop up with score
+
+//timer
+
+//new game, clear tracker, start from first question
+
+//skip ahead, go back option
+
+//if skip then can come back at end to answer quetion 
+
